7f6accb9a9fcd9ed6de1bac6214de497ebefd745
nicomp42 <nicholdw@ucmail.uc.edu>
2024-10-31 09:36:46 -0400

    Add project files.

---
+++ IS3050-002 Group Collaboration Project/Cat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS3050_002_Group_Collaboration_Project
+{
+    internal class Cat
+    {
+    }
+}
---
+++ IS3050-002 Group Collaboration Project/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS3050_002_Group_Collaboration_Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare and instantiate your classes
+            // invoke your methods
+        }
+    }
+}
